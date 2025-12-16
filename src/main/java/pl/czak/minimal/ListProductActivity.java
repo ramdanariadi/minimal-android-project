9eede4ed670d01b5b3ec1dabcd83e18a7cd645d6
Ramdan Ariadi <ramadanariadi@gmail.com>
2025-12-16 20:25:48 +0800

    chore: setup

---
+++ src/main/java/pl/czak/minimal/ListProductActivity.java
@@ -0,0 +1,11 @@
+package pl.czak.minimal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListProductActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- src/main/java/pl/czak/minimal/MainActivity.java
+++ src/main/java/pl/czak/minimal/MainActivity.java
@@ -2,6 +2,7 @@ package pl.czak.minimal;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity
@@ -9,10 +10,10 @@ public class MainActivity extends Activity
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        TextView label = new TextView(this);
-        label.setText("Hello world!");
-
-        setContentView(label);
+        Button btn1 = findViewById(R.id.btn1);
+        TextView textView = findViewById(R.id.text1);
+        btn1.setOnClickListener(view -> textView.setText("Button clicked!"));
     }
 }

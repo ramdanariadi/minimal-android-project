15ec87c8d77ffd87408ec662546121feba40d938
Ramdan Ariadi <ramadanariadi@gmail.com>
2025-12-17 22:45:24 +0800

    feat: product list activity

--- src/main/java/pl/czak/minimal/ListProductActivity.java
+++ src/main/java/pl/czak/minimal/ListProductActivity.java
@@ -2,10 +2,20 @@ package pl.czak.minimal;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import pl.czak.minimal.adapter.ProductListAdapter;
+import pl.czak.minimal.dto.Product;
 
 public class ListProductActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_product);
+
+        ProductListAdapter adapter = new ProductListAdapter(Product.createDummy());
+        RecyclerView recyclerView = findViewById(R.id.rc_product);
+        recyclerView.setLayoutManager(new android.support.v7.widget.LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
--- src/main/java/pl/czak/minimal/MainActivity.java
+++ src/main/java/pl/czak/minimal/MainActivity.java
@@ -1,6 +1,7 @@
 package pl.czak.minimal;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,7 +14,9 @@ public class MainActivity extends Activity
         setContentView(R.layout.activity_main);
 
         Button btn1 = findViewById(R.id.btn1);
-        TextView textView = findViewById(R.id.text1);
-        btn1.setOnClickListener(view -> textView.setText("Button clicked!"));
+        btn1.setOnClickListener(view -> {
+            Intent intent = new Intent(this, ListProductActivity.class);
+            startActivity(intent);
+        });
     }
 }

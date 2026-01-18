6216581fb9c2bd32d85d3f8e58b3e9a40b3f3f8d
Ramdan Ariadi <ramadanariadi@gmail.com>
2026-01-18 23:18:01 +0800

    feat: Add TransactionApiService

---
+++ src/main/java/pl/czak/minimal/service/TransactionApiService.java
@@ -0,0 +1,9 @@
+package pl.czak.minimal.service;
+
+import retrofit2.Call;
+import retrofit2.http.POST;
+
+public interface TransactionApiService {
+    @POST("/api/v1/transaction")
+    Call<Void> order();
+}

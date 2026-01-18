e4f442c4250afb5e28a7018c48970254733a96dc
Ramdan Ariadi <ramadanariadi@gmail.com>
2026-01-18 23:17:49 +0800

    feat: Add ProductApiService interface for Retrofit API calls

---
+++ src/main/java/pl/czak/minimal/service/ProductApiService.java
@@ -0,0 +1,10 @@
+package pl.czak.minimal.service;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ProductApiService {
+
+    @GET("/api/v1/product")
+    Call<Void> getProducts();
+}
